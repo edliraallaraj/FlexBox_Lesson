9497de8bceedd5dc5d6253af0c65e7ec7b9e8172
Edlira Allaraj <eallaraj@ikub.local>
2022-07-02 10:42:56 +0200

    changes in flexbox

---
+++ script.js
@@ -0,0 +1,12 @@
+// First element child 
+// let firstStudent = document.getElementById("myList").firstElementChild.innerHTML;
+// document.getElementById("first-el").innerHTML = firstStudent;
+
+// First element child with tag name
+
+// let tagNameEl = document.getElementById("myDiv").firstElementChild.tagName;
+// document.getElementById("tag-name").innerHTML = tagNameEl;
+
+const firstListElement = document.getElementById('dList').firstElementChild.innerHTML;
+// console.log(firstListElement)
+document.getElementById('firstEl').innerHTML = firstListElement;
